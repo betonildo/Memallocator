c559d414883525a866a2d74c4997a03bb8c2551d
grpse <gilbertoribeiropazdarosa@gmail.com>
2017-10-04 17:27:43 -0300

    [add] allocator for static call

--- include/Allocator.h
+++ include/Allocator.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "PtrHandle.h"
+
+template <class AllocatorType>
+class Allocator{
+
+public:
+    template<class PointerType>
+    inline PtrHandle<PointerType> allocateWithHandle() {
+        return allocator.allocateWithHandle<PointerType>();
+    }
+
+private:
+    AllocatorType allocator;
+};
